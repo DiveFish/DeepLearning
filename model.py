93318261d9437079037e4d65de9c8e7b70e2d915
Blubberli <neele.witte@student.uni-tuebingen.de>
2017-07-03 21:30:06 +0200

    comments and optimal parameters in model

--- model.py
+++ model.py
@@ -20,20 +20,28 @@ class Model:
         input_size = batch.shape[2]
         label_size = label_batch.shape[2]
 
+        'placeholder for input data'
         self._x = tf.placeholder(tf.int32, shape=[batch_size, input_size])
 
+        'if char_embeds is true a variable for char embeddings is created. Embedding dimensions are set to 200' \
+        'The embedding matrix contains one embedding per character of prefix and suffic for every instance of input'
         if use_char_embeds:
             embedding_dims = 200
             self._embeddings = embeddings = tf.get_variable("embeddings", shape = [n_chars, embedding_dims])
             embeddings = tf.nn.embedding_lookup(embeddings, self._x)
             embeddings = tf.reshape(embeddings, [batch_size, (input_size * embedding_dims)])
+
+            'If char_embeds is false a onehot representation of every character of prefix and suffix of the input is created' \
+            'The onehot vectors for all characters are concatenated in order to retrieve the feature vector '
         else:
             x_hot = tf.one_hot(self._x, n_chars)
             x_hot = tf.reshape(x_hot, [batch_size, (input_size * n_chars)])
 
         if phase != Phase.Predict:
+            'a placeholder for the label of the training data is created'
             self._y = tf.placeholder(tf.float32, shape=[batch_size, label_size])
 
+        'data is send through the hidden layers.'
         if (use_char_embeds):
             hidden = self.hidden_layers(phase, embeddings, hidden_sizes)
         else:
@@ -44,27 +52,32 @@ class Model:
 
         logits = tf.matmul(hidden, w) + b
 
-        # loss in train and validation phase
+        'loss is calculated in train and validation phase. loss function is softmax_cross_entropy'
         if phase != Phase.Predict:
             losses = tf.nn.softmax_cross_entropy_with_logits(labels = self._y, logits = logits)
             self._loss = loss = tf.reduce_sum(losses)
 
         if phase == Phase.Train:
+            'best optimizer: AdagradOptimizer with a learning rate of 0.005'
             self._train_op = tf.train.AdagradOptimizer(0.005).minimize(loss)
         else:
-            self._probs = probs = tf.nn.softmax(logits) # train on softmax -> use softmax here too; prob dist over tags
-            #self._labels = tf.cast(tf.round(probs), tf.float32) # rounds probs <0.5 to 0 and >0.5 to 1
-
+            'probability distribution over tags is calculated with softmax function'
+            self._probs = probs = tf.nn.softmax(logits)
         if phase != Phase.Train:
+            'accuracy is calculated in validation and prediction phase'
             correct = tf.equal(tf.argmax(self.y, axis=1), tf.argmax(probs, axis=1))
             self._accuracy = tf.reduce_mean(tf.cast(correct, tf.float32))
 
+    'takes input, applies nonlinear function and multiplication with weighvector, adding bias' \
+    'returns the result after applieng all hidden layers'
     def hidden_layers(self, phase, input_layer, hidden_sizes):
         for (i, hidden_size) in enumerate(hidden_sizes):
             if phase == Phase.Train:
-                input_layer = tf.nn.dropout(input_layer, 0.90)
+                'using dropout for regularization, 0.7 works best'
+                input_layer = tf.nn.dropout(input_layer, 0.70)
             W = tf.get_variable("W_hidden_%d" % i, shape = [input_layer.shape[1], hidden_size])
             b = tf.get_variable("b_hidden_%d" % i, shape = [hidden_size])
+            'best activation function compared to others: ReLU'
             hidden_outputs = tf.nn.relu(tf.matmul(input_layer, W) + b)
             input_layer = hidden_outputs
         return input_layer
