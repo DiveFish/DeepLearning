0c5925d0d0928da32102f002d1f6656b14cd7e35
DiveFish <DiveFish@users.noreply.github.com>
2017-08-01 13:14:52 +0200

    Final config

--- rnn/config.py
+++ rnn/config.py
@@ -1,7 +1,7 @@
 class DefaultConfig:
-    n_epochs = 50
+    n_epochs = 40
     batch_size = 512
-    input_dropout = 0.95
-    hidden_dropout = 0.95
+    input_dropout = 0.8
+    hidden_dropout = 0.85
     max_timesteps = 20
 
