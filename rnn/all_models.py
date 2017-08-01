446475edec73a7dc9d288c3141411854f6203c25
DiveFish <DiveFish@users.noreply.github.com>
2017-08-01 13:14:23 +0200

    Final models

--- rnn/all_models.py
+++ rnn/all_models.py
@@ -1,3 +1,6 @@
+#Authors: Neele Witte, 4067845; Patricia Fischer, 3928367
+#Honor Code:  We pledge that this program represents our own work.
+
 from enum import Enum
 
 import tensorflow as tf
@@ -28,19 +31,7 @@ class Model:
         label_size = label_batch.shape[2]
         hidden_layers = 200
         embedding_size = 50
-        num_of_lstms = 2    #for stacked lstm
-
-        print("---------------")
-        print("network: bidir="+str(use_bidir)+" lstm="+str(use_lstm)+" stacked="+str(use_stacked))
-        print("epochs: "+str(config.n_epochs))
-        print("max timesteps: "+str(config.max_timesteps))
-        print("embedding/ input dropout: "+str(config.input_dropout))
-        print("state/ hidden dropout: "+str(config.hidden_dropout))
-        print("hidden layers: "+str(hidden_layers))
-        print("embedding size: "+str(embedding_size))
-        print("number of stacks: "+str(num_of_lstms))
-        print("start learning rate: 0.01")
-        print("optimizer: Adam")
+        num_of_lstms = 2    # for stacked lstm
 
         # The integer-encoded words. input_size is the (maximum) number of
         # time steps.
@@ -48,7 +39,7 @@ class Model:
         self._embeddings = embeddings = tf.get_variable("embeddings", shape=[n_chars, embedding_size])
         embeddings = tf.nn.embedding_lookup(embeddings, self._x)
         if phase == Phase.Train:
-            embeddings = tf.contrib.layers.dropout(embeddings, keep_prob=config.input_dropout)
+            embeddings = tf.nn.dropout(embeddings, keep_prob=config.input_dropout)
 
         # This tensor provides the actual number of time steps for each
         # instance.
@@ -56,22 +47,33 @@ class Model:
 
         # The label distribution.
         if phase != Phase.Predict:
-            self._y = tf.placeholder(
-                tf.float32, shape=[batch_size, label_size])
+            self._y = tf.placeholder(tf.float32, shape=[batch_size, label_size])
 
-        if use_bidir:
+        if use_bidir:   # best model: bidirectional
             forward_cell = rnn.GRUCell(hidden_layers)
             backward_cell = rnn.GRUCell(hidden_layers)
             if phase == Phase.Train:
                 forward_cell = rnn.DropoutWrapper(forward_cell, state_keep_prob=config.hidden_dropout,
-                                      output_keep_prob=config.hidden_dropout)
+                                                  output_keep_prob=config.hidden_dropout)
                 backward_cell = rnn.DropoutWrapper(backward_cell, state_keep_prob=config.hidden_dropout,
-                                      output_keep_prob=config.hidden_dropout)
+                                                   output_keep_prob=config.hidden_dropout)
 
             _, hidden = tf.nn.bidirectional_dynamic_rnn(forward_cell, backward_cell, embeddings,
                                                         sequence_length=self._lens, dtype=tf.float32)
             hidden_1, hidden_2 = hidden
             hidden = tf.concat([hidden_1, hidden_2], 1)
+
+        elif use_stacked:   # second best model: stacked
+            stacked_cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(hidden_layers, reuse=tf.get_variable_scope().reuse) for _ in range(num_of_lstms)], state_is_tuple=True)
+            if phase == Phase.Train:
+                stacked_cell = rnn.DropoutWrapper(stacked_cell, state_keep_prob=config.hidden_dropout,
+                                                  output_keep_prob=config.hidden_dropout)
+            _, hiddens = tf.nn.dynamic_rnn(stacked_cell, embeddings, sequence_length=self._lens, dtype=tf.float32)
+            # hiddens = hidden1, hidden2, hidden3,.... (num_of_lstms)
+
+            _, hidden_orig = hiddens[0]
+            for _, hidden in hiddens[1:]:
+                hidden_orig = tf.concat([hidden_orig, hidden], 1)
 
         elif use_lstm:
             lstm_cell = tf.contrib.rnn.BasicLSTMCell(hidden_layers, reuse=tf.get_variable_scope().reuse)
@@ -80,23 +82,11 @@ class Model:
                                                output_keep_prob=config.hidden_dropout)
             _, (_, hidden) = tf.nn.dynamic_rnn(lstm_cell, embeddings, sequence_length=self._lens, dtype=tf.float32)
 
-        elif use_stacked:
-            stacked_cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(hidden_layers, reuse=tf.get_variable_scope().reuse) for _ in range(num_of_lstms)], state_is_tuple=True)
-            if phase == Phase.Train:
-                stacked_cell = rnn.DropoutWrapper(stacked_cell, state_keep_prob=config.hidden_dropout,
-                                                  output_keep_prob=config.hidden_dropout)
-            _, hiddens = tf.nn.dynamic_rnn(stacked_cell, embeddings, sequence_length=self._lens, dtype=tf.float32)
-            # hiddens = hidden1, hidden2, hidden3,....
-
-            _, hidden_orig = hiddens[0]
-            for _, hidden in hiddens[1:]:
-                hidden_orig = tf.concat([hidden_orig, hidden], 1)
-
         else:
             gru_cell = rnn.GRUCell(hidden_layers)
             if phase == Phase.Train:
                 gru_cell = rnn.DropoutWrapper(gru_cell, state_keep_prob=config.hidden_dropout,
-                                                  output_keep_prob=config.hidden_dropout)
+                                              output_keep_prob=config.hidden_dropout)
             _, hidden = tf.nn.dynamic_rnn(gru_cell, embeddings, sequence_length=self._lens, dtype=tf.float32)
 
         w = tf.get_variable("w", shape=[hidden.shape[1], label_size])
@@ -104,8 +94,7 @@ class Model:
         logits = tf.matmul(hidden, w) + b
 
         if phase == Phase.Train or Phase.Validation:
-            losses = tf.nn.softmax_cross_entropy_with_logits(
-                labels=self._y, logits=logits)
+            losses = tf.nn.softmax_cross_entropy_with_logits(labels=self._y, logits=logits)
             self._loss = loss = tf.reduce_sum(losses)
 
         if phase == Phase.Train:
