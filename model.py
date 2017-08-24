55fbe11b743f56fd23f50be48ef48c64f996dc98
DiveFish <divefish@users.noreply.github.com>
2017-08-24 20:24:28 +0200

    Removed wordclass-rnn-specific code; added precision, recall, f1-score

--- model.py
+++ model.py
@@ -17,14 +17,12 @@ class Model:
     def __init__(
             self,
             config,
+            label_vectors, # dictionary of labels and their one-hot vector representation
             batch,
             lens_batch,
             label_batch,
             n_chars,
-            phase=Phase.Predict,
-            use_bidir=True,
-            use_lstm=False,
-            use_stacked=False):
+            phase=Phase.Predict):
         num_of_batches = len(lens_batch)
         batch_size = batch.shape[1]
         input_size = batch.shape[2]
@@ -47,47 +45,20 @@ class Model:
 
         # The label distribution.
         if phase != Phase.Predict:
-            self._y = tf.placeholder(tf.float32, shape=[batch_size, label_size])
+            self._y = tf.placeholder(tf.float32, shape=[batch_size])
 
-        if use_bidir:   # best model: bidirectional
-            forward_cell = rnn.GRUCell(hidden_layers)  # instead use rnn.BasicLSTMCell
-            backward_cell = rnn.GRUCell(hidden_layers)
-            if phase == Phase.Train:
-                forward_cell = rnn.DropoutWrapper(forward_cell, state_keep_prob=config.hidden_dropout,
-                                                  output_keep_prob=config.hidden_dropout)
-                backward_cell = rnn.DropoutWrapper(backward_cell, state_keep_prob=config.hidden_dropout,
-                                                   output_keep_prob=config.hidden_dropout)
-
-            _, hidden = tf.nn.bidirectional_dynamic_rnn(forward_cell, backward_cell, embeddings,
-                                                        sequence_length=self._lens, dtype=tf.float32)
-            hidden_1, hidden_2 = hidden
-            hidden = tf.concat([hidden_1, hidden_2], 1)
-
-        elif use_stacked:   # second best model: stacked
-            stacked_cell = tf.contrib.rnn.MultiRNNCell([tf.contrib.rnn.BasicLSTMCell(hidden_layers, reuse=tf.get_variable_scope().reuse) for _ in range(num_of_lstms)], state_is_tuple=True)
-            if phase == Phase.Train:
-                stacked_cell = rnn.DropoutWrapper(stacked_cell, state_keep_prob=config.hidden_dropout,
-                                                  output_keep_prob=config.hidden_dropout)
-            _, hiddens = tf.nn.dynamic_rnn(stacked_cell, embeddings, sequence_length=self._lens, dtype=tf.float32)
-            # hiddens = hidden1, hidden2, hidden3,.... (num_of_lstms)
-
-            _, hidden_orig = hiddens[0]
-            for _, hidden in hiddens[1:]:
-                hidden_orig = tf.concat([hidden_orig, hidden], 1)
-
-        elif use_lstm:
-            lstm_cell = tf.contrib.rnn.BasicLSTMCell(hidden_layers, reuse=tf.get_variable_scope().reuse)
-            if phase == Phase.Train:
-                lstm_cell = rnn.DropoutWrapper(lstm_cell, state_keep_prob=config.hidden_dropout,
-                                               output_keep_prob=config.hidden_dropout)
-            _, (_, hidden) = tf.nn.dynamic_rnn(lstm_cell, embeddings, sequence_length=self._lens, dtype=tf.float32)
-
-        else:
-            gru_cell = rnn.GRUCell(hidden_layers)
-            if phase == Phase.Train:
-                gru_cell = rnn.DropoutWrapper(gru_cell, state_keep_prob=config.hidden_dropout,
+        forward_cell = tf.contrib.rnn.BasicLSTMCell(hidden_layers)  # instead use rnn.BasicLSTMCell
+        backward_cell = tf.contrib.rnn.BasicLSTMCell(hidden_layers)
+        if phase == Phase.Train:
+            forward_cell = rnn.DropoutWrapper(forward_cell, state_keep_prob=config.hidden_dropout,
                                               output_keep_prob=config.hidden_dropout)
-            _, hidden = tf.nn.dynamic_rnn(gru_cell, embeddings, sequence_length=self._lens, dtype=tf.float32)
+            backward_cell = rnn.DropoutWrapper(backward_cell, state_keep_prob=config.hidden_dropout,
+                                               output_keep_prob=config.hidden_dropout)
+
+        _, hidden = tf.nn.bidirectional_dynamic_rnn(forward_cell, backward_cell, embeddings,
+                                                    sequence_length=self._lens, dtype=tf.float32)
+        hidden_1, hidden_2 = hidden
+        hidden = tf.concat([hidden_1, hidden_2], 1)
 
         w = tf.get_variable("w", shape=[hidden.shape[1], label_size])
         b = tf.get_variable("b", shape=[1])
@@ -107,19 +78,26 @@ class Model:
             self._probs = probs = tf.nn.softmax(logits)
 
         if phase == Phase.Validation:
-            # Highest probability labels of the gold data.
-            hp_labels = tf.argmax(self.y, axis=1)
+            # Highest probability labels of the gold data: self.y
 
-            # Predicted labels
-            labels = tf.argmax(logits, axis=1)
+            # Predicted labels: logits
 
-            correct = tf.equal(hp_labels, labels)
+            # TODO: include label dict
+            # accuracy; for precision, consider only the named-entity labels, not O (outisde of named entity)
+            correct = tf.equal(self._y, logits)
             correct = tf.cast(correct, tf.float32)
-            self._accuracy = tf.reduce_mean(correct)
+            self._precision = tf.reduce_mean(correct)
 
-    @property
-    def accuracy(self):
-        return self._accuracy
+            # TODO: include label dict
+            # recall: correctly labeled named entities vs. correctly and erroneously labeled named entities
+            correct = tf.equal(self._y, logits)
+            correct = tf.cast(correct, tf.float32)
+            incorrect = tf.not_equal(self._y, logits)
+            incorrect = tf.cast(incorrect, tf.float32)
+            self._recall = correct / (correct+incorrect)
+
+            # f1 score: 2∗precision∗recall/(precision+recall)
+            self._f1_score = 2*self._precision*self._recall/(self._precision+self._recall)
 
     @property
     def embeddings(self):
@@ -134,8 +112,16 @@ class Model:
         return self._loss
 
     @property
+    def precision(self):
+        return self._precision
+
+    @property
     def probs(self):
         return self._probs
+
+    @property
+    def recall(self):
+        return self._recall
 
     @property
     def train_op(self):
