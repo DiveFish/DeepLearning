dc8389fd2805f2a9806f801bdd159ce901d5de4e
Neele <Neele.witte@student.uni-tuebingen.de>
2017-09-21 19:38:21 +0200

    labels = integers, embeddings = lookup method, training-test = 25.000 sentence and 3000

--- chunker.py
+++ chunker.py
@@ -15,3 +15,42 @@ class Chunker:
                 return True
             else:
                 return False
+
+    def extract_namedEntites(sequence):
+        chunks = []
+        chunk = []
+        for tagindex in range(len(sequence)):
+            if sequence[tagindex].startswith('B') and len(chunk)==0:
+                chunk.append(tagindex)
+                chunk.append(sequence[tagindex])
+            elif sequence[tagindex].startswith('I') and len(chunk)==0:
+                chunk.append(tagindex)
+                chunk.append(sequence[tagindex])
+            elif sequence[tagindex].startswith('I'):
+                chunk.append(sequence[tagindex])
+            elif sequence[tagindex].startswith('O') and len(chunk)!=0:
+                chunk.append(tagindex)
+                chunks.append(chunk)
+                chunk = []
+            elif sequence[tagindex].startswith('B') and len(chunk)!=0:
+                chunk.append(tagindex)
+                chunks.append(chunk)
+                chunk = []
+                chunk.append(tagindex)
+                chunk.append(sequence[tagindex])
+        return chunks
+
+    if __name__ == '__main__':
+        seq = ['B-loc', 'B-loc', 'I-o', 'O', 'O', 'B-I', 'I-o', 'I-o', 'B-o', 'O']
+        seq2 = ['B-loc', 'B-loc', 'B-loc', 'I-o', 'O', 'O', 'B-I', 'I-o', 'I-o', 'B-o', 'O']
+
+
+        k = extract_namedEntites(seq2)
+        k2 = extract_namedEntites(seq)
+        for ner in k:
+            if (ner in k2):
+                print(ner)
+
+        print(k)
+        print(k2)
+
--- scorer.py
+++ scorer.py
@@ -3,54 +3,40 @@ from chunker import Chunker
 
 class Scorer:
 
-
     def scores(self, viterbi_sequences, id_to_tag_dict, validation_labels, batch):
         # Get performance of model: precision, recall, f1-score
         found_guessed = 0
         found_correct = 0
         correct_chunks = 0
+        TP = 0
+        FP = 0
         chunker = Chunker()
         counter =0
         for seq in range(len(viterbi_sequences)):
             tags = viterbi_sequences[seq]
-            inside_chunk = False
-            for tag_idx in range(len(tags)):
-                guessed_tag = id_to_tag_dict.get(tags[tag_idx])
-                y_tag = id_to_tag_dict.get(validation_labels[batch][seq][tag_idx])
-                sequence_end = False
-                if  tag_idx != 0:
-                    sequence_end = True
-                    previous_guessed = id_to_tag_dict.get(tags[tag_idx-1])
-                    previous_gold = id_to_tag_dict.get(validation_labels[batch][seq][tag_idx-1])
+            guessedtags = validation_labels[batch][seq]
+            golden = guessedtags[0:len(tags)]
+            goldentags = [id_to_tag_dict[i] for i in golden]
+            viterbittags = [id_to_tag_dict[k] for k in tags]
+            golden_entities = extract_namedEntites(goldentags)
+            viterbi_entities = extract_namedEntites(viterbittags)
+            found_guessed+=len(viterbi_entities)
+            found_correct+=len(golden_entities)
+            for ner in viterbi_entities:
+                if (ner in golden_entities):
+                    correct_chunks+=1
+                    TP+=1
                 else:
-                    previous_guessed = id_to_tag_dict.get(tags[tag_idx])
-                    previous_gold = id_to_tag_dict.get(validation_labels[batch][seq][tag_idx])
-                if chunker.chunk_start(guessed_tag):
-                    found_guessed += 1
-                    inside_chunk = True
-                    if guessed_tag == y_tag:
-                        in_correct = True
-                if chunker.chunk_start(y_tag):
-                    found_correct += 1
-                # For each tag check whether it matches the gold standard tag
-                # in_correct true if all tags in chunk processed so far matched
-                if inside_chunk and (guessed_tag == y_tag) and in_correct:
-                    continue
-                else:
-                    in_correct = False
-
-                if chunker.chunk_end(previous_guessed,guessed_tag, sequence_end) and chunker.chunk_end(previous_gold,y_tag, sequence_end) and in_correct:
-                    correct_chunks += 1
-                    # Reset values for next chunk
-                    inside_chunk = False
-                    in_correct = False
+                    FP+=1
         f1_score = 0
         precision = 0
+       
+        print(correct_chunks)
         if found_guessed > 0:
-            precision = 100.00 * (float(correct_chunks) / float(found_guessed))
+            precision = float(correct_chunks) / float(TP+FP)
 
         if found_correct > 0:
-            recall = 100.00 * (float(correct_chunks) / float(found_correct))
+            recall = float(correct_chunks) / float(found_correct)
 
         if (precision > 0) and (recall > 0):
             f1_score = 2.0 * precision * recall / (precision + recall)
@@ -58,3 +44,26 @@ class Scorer:
 
         return precision, recall, f1_score
 
+def extract_namedEntites(sequence):
+    chunks = []
+    chunk = []
+    for tagindex in range(len(sequence)):
+        if sequence[tagindex].startswith('B') and len(chunk)==0:
+            chunk.append(tagindex)
+            chunk.append(sequence[tagindex])
+        elif sequence[tagindex].startswith('I') and len(chunk)==0:
+            chunk.append(tagindex)
+            chunk.append(sequence[tagindex])
+        elif sequence[tagindex].startswith('I'):
+            chunk.append(sequence[tagindex])
+        elif sequence[tagindex].startswith('O') and len(chunk)!=0:
+            chunk.append(tagindex)
+            chunks.append(chunk)
+            chunk = []
+        elif sequence[tagindex].startswith('B') and len(chunk)!=0:
+            chunk.append(tagindex)
+            chunks.append(chunk)
+            chunk = []
+            chunk.append(tagindex)
+            chunk.append(sequence[tagindex])
+    return chunks
